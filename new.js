e79784887af8563769839f545d537c4e1d4078ff
tanvirislamrakib <tanvirislamrakib93@gmail.com>
2024-01-07 16:41:06 +0600

    difference

---
+++ new.js
@@ -0,0 +1,16 @@
+// var diclayer korle variable re diclaer kora jay
+var num = 20;
+var num = 50;
+console.log(num);
+
+
+// let dicaler er maddhome variablke re assing kora jay
+// let ekbar diclaier korle ditiyo bar same variable e diclaer kora jay na
+let num1 = 60;
+num1 = 70;
+console.log(num1);
+
+//const diclaer er maddhome kono variable re-assign and re-diclear kora jay na
+
+const ami = "My name is Rakib";
+console.log(ami);
